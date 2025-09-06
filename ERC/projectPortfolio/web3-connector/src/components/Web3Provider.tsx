f603f40bd375bf7f3d7a7387a957129b7ba66a62
fpaopao <1161108291@qq.com>
2025-09-06 21:16:25 +0800

    删除无用代码

--- ERC/projectPortfolio/web3-connector/src/components/Web3Provider.tsx
+++ ERC/projectPortfolio/web3-connector/src/components/Web3Provider.tsx
@@ -1,13 +1,15 @@
-'use client';
+"use client";
 
-import { createConfig, WagmiProvider } from 'wagmi';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { http } from 'viem';
-import { sepolia, mainnet, polygon, arbitrum, goerli } from 'wagmi/chains';
-import { metaMask, injected, walletConnect, coinbaseWallet } from 'wagmi/connectors';
-import { ReactNode } from 'react';
+import { createConfig, WagmiProvider } from "wagmi";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { http } from "viem";
+import { sepolia, mainnet, polygon, arbitrum, goerli } from "wagmi/chains";
+import { metaMask, injected, walletConnect } from "wagmi/connectors";
+import { ReactNode } from "react";
 
-export const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || '4eee412f87919d54aa6c870ab6eca637'
+export const projectId =
+  process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID ||
+  "4eee412f87919d54aa6c870ab6eca637";
 
 const queryClient = new QueryClient();
 
@@ -17,15 +19,11 @@ const chains = [sepolia, mainnet, polygon, arbitrum, goerli] as const;
 // 创建wagmi配置
 export const config = createConfig({
   chains,
-  connectors: [
-    injected(),
-    metaMask(),
-    walletConnect({ projectId: projectId }),
-  ],
+  connectors: [injected(), metaMask(), walletConnect({ projectId: projectId })],
   transports: {
-    [sepolia.id]: http('https://eth-sepolia.api.onfinality.io/public'),
-    [mainnet.id]: http('https://mainnet.rpc.url'),
-    [polygon.id]: http('https://polygon.rpc.url'),
+    [sepolia.id]: http("https://eth-sepolia.api.onfinality.io/public"),
+    [mainnet.id]: http("https://mainnet.rpc.url"),
+    [polygon.id]: http("https://polygon.rpc.url"),
     [arbitrum.id]: http(),
     [goerli.id]: http(),
   },
@@ -34,9 +32,7 @@ export const config = createConfig({
 export default function Web3Provider({ children }: { children: ReactNode }) {
   return (
     <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        {children}
-      </QueryClientProvider>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </WagmiProvider>
   );
 }
